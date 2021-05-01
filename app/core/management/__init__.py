d126daa7840743c51df3aa7c5cb0a46751442946
Dennis Joel <dennismwagiru@gmail.com>
2021-05-01 23:31:17 +0300

    Configured postgres db

---
+++ app/core/management/__init__.py
@@ -0,0 +1,5 @@
+"""
+ @created 01/05/2021 - 11:16 PM
+ @project recipe-app-api
+ @author dennis joel
+"""
---
+++ app/core/management/commands/__init__.py
@@ -0,0 +1,5 @@
+"""
+ @created 01/05/2021 - 11:17 PM
+ @project recipe-app-api
+ @author dennis joel
+"""
